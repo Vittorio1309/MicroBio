7e3cf00de55b90522bcc5670fe51902a5758876a
Daniel Todao <danielhtodao@gmail.com>
2026-04-02 00:41:57 -0300

    Pagina choose finalizada

--- project/front-end/js/choose.jsx
+++ project/front-end/js/choose.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import backgroundImageWithOverlay from "./background-image-with-overlay.png";
-import icon2 from "./icon-2.svg";
-import icon from "./icon.svg";
-import microbioHero1 from "./microbio-hero-1.png";
+import backgroundImageWithOverlay from "../img/background-image-with-overlay.png";
+import icon2 from "../img/icon-2.svg";
+import icon from "../img/Icon.svg";
+import microbioHero1 from "../img/microbio-hero-1.png";
 import "../css/choose.css";
 
 export const Body = () => {
+  const agroUrl = "/agro.html"; // altere para a página do agro quando estiver pronta
+  const saudeUrl = "https://microbio.uniexames.com.br/cms/inicio"; // altere para a URL do portal Saúde
+
   return (
     <div className="body">
       <div className="main">
+        {/* SEÇÃO AGRONEGÓCIO */}
         <div className="div">
           <img
             className="background-image"
             alt="Background image"
             src={backgroundImageWithOverlay}
           />
-
           <div className="overlay" />
-
           <div className="decorative-organic" />
-
           <div className="content">
             <div className="heading">
               <div className="text">Agronegócio</div>
             </div>
-
             <div className="container">
               <p className="text-wrapper">
                 Soluções biológicas inovadoras para
@@ -34,33 +34,28 @@ export const Body = () => {
                 solo de forma sustentável.
               </p>
             </div>
-
-            <div className="background">
+            <a className="background" href={agroUrl}>
               <div className="overlay-shadow" />
-
               <div className="text-2">Acessar Plataforma Agro</div>
-
               <div className="icon-wrapper">
                 <img className="icon" alt="Icon" src={icon} />
               </div>
-            </div>
+            </a>
           </div>
         </div>
 
+        {/* SEÇÃO SAÚDE (CORRIGIDA) */}
         <div className="div">
+          <img
+            className="background-image"
+            alt="Microbio hero"
+            src={microbioHero1}
+          />
           <div className="overlay-2" />
-
           <div className="content-2">
-            <img
-              className="microbio-hero"
-              alt="Microbio hero"
-              src={microbioHero1}
-            />
-
             <div className="heading">
               <div className="text-wrapper-2">Saúde</div>
             </div>
-
             <div className="div-wrapper">
               <p className="p">
                 Ciência avançada em análises
@@ -70,33 +65,28 @@ export const Body = () => {
                 para precisão clínica.
               </p>
             </div>
-
-            <div className="background-2">
+            <a className="background-2" href={saudeUrl} >
               <div className="overlay-shadow" />
-
               <div className="text-3">Acessar Portal Saúde</div>
-
               <div className="icon-wrapper">
-                <img className="icon" alt="Icon" src={image} />
+                <img className="icon" alt="Icon" src={icon} />
               </div>
-            </div>
+            </a>
           </div>
         </div>
       </div>
 
+      {/* ELEMENTOS CENTRAIS E CABEÇALHO */}
       <div className="center-badge-the">
         <div className="background-3">
           <div className="overlay-shadow-2" />
-
           <div className="img-wrapper">
             <img className="img" alt="Icon" src={icon2} />
           </div>
         </div>
-
         <div className="margin">
           <div className="background-border">
             <div className="overlay-shadow-3" />
-
             <div className="text-4">SELECIONE</div>
           </div>
         </div>
@@ -105,7 +95,6 @@ export const Body = () => {
       <div className="header-top">
         <div className="overlay-border">
           <div className="overlay-shadow-3" />
-
           <div className="text-5">MicroBio</div>
         </div>
       </div>
