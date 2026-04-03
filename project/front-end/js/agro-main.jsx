d97a5a36450c478c34d1b216c14a8219bddd93d0
Daniel Todao <danielhtodao@gmail.com>
2026-04-02 21:37:04 -0300

    Teste finais

--- project/front-end/js/agro-main.jsx
+++ project/front-end/js/agro-main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Section } from './agro.jsx';
+import { Nav, Section } from './agro.jsx';
 import '../css/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Nav />
     <Section />
   </React.StrictMode>
 );
--- project/front-end/js/agro.jsx
+++ project/front-end/js/agro.jsx
@@ -2,6 +2,54 @@ import React from "react";
 import arrowIcon from "../img/icon-2.svg";
 import "../css/agro.css";
 
+
+export const Nav = () => {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="nav">
+      <div className="container">
+        <div className="text-wrapper">
+          <div className="text">MicroBio</div>
+        </div>
+
+        <div className="div">
+          <button className="link" onClick={() => scrollToSection('inicio')}>
+            <div className="text-2">Início</div>
+          </button>
+
+          <button className="nav-link" onClick={() => scrollToSection('servicos')}>
+            <div className="text-3">Serviços</div>
+          </button>
+
+          <button className="nav-link" onClick={() => scrollToSection('sobre')}>
+            <div className="text-4">Sobre</div>
+          </button>
+
+          <button className="nav-link" onClick={() => scrollToSection('contato')}>
+            <div className="text-5">Contato</div>
+          </button>
+        </div>
+
+        <div className="nav-actions">
+          <button className="button-login">
+            <div className="text">Login</div>
+          </button>
+
+          <button className="button-orcamento">
+            <div className="div">Solicitar Orçamento</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const Section = () => {
   return (
     <div className="section">
@@ -35,6 +83,70 @@ export const Section = () => {
           </button>
         </div>
       </div>
+    </div>
+  );
+};
+
+import vector from "../img/svg-1.svg";
+import vector2 from "../img/svg-2.svg";
+import vector3 from "..img/svg-3.svg";
+
+
+export const BackgroundBorder = () => {
+  return (
+    <div className="background-border">
+      <div className="overlay-shadow" />
+
+      <div className="background">
+        <div className="SVG">
+          <img className="vector" alt="Vector" src={vector} />
+
+          <img className="img" alt="Vector" src={image} />
+
+          <img className="vector-2" alt="Vector" src={vector2} />
+        </div>
+      </div>
+
+      <div className="heading-an-lises">Análises Precisas</div>
+
+      <p className="laudos-t-cnicos-com">
+        Laudos técnicos com
+        <br />
+        metodologias reconhecidas e<br />
+        certificadas.
+      </p>
+
+      <div className="SVG-wrapper">
+        <div className="SVG">
+          <img className="vector-3" alt="Vector" src={vector3} />
+
+          <img className="vector-4" alt="Vector" src={vector4} />
+        </div>
+      </div>
+
+      <div className="heading">Sustentabilidade</div>
+
+      <p className="dados-que-otimizam-o">
+        Dados que otimizam o uso de
+        <br />
+        insumos e preservam o solo.
+      </p>
+
+      <div className="div-wrapper">
+        <div className="SVG">
+          <img className="vector-5" alt="Vector" src={vector5} />
+
+          <img className="vector-6" alt="Vector" src={vector6} />
+        </div>
+      </div>
+
+      <div className="heading-confian-a">Confiança</div>
+
+      <p className="mais-de">
+        Mais de 1.450 propriedades
+        <br />
+        monitoradas no último ano.
+      </p>
     </div>
   );
 };
