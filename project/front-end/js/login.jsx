34b9d75e4ed50d29a0e556c11ec22c2f8b539179
danieltodao <danielhtodao@gmail.com>
2026-04-23 21:47:34 -0300

    feat: tela login V1

---
+++ project/front-end/js/login.jsx
@@ -0,0 +1,92 @@
+import icon from "./icon.svg";
+import icon2 from "./icon-2.svg";
+import icon3 from "./icon-3.svg";
+import image from "./image.svg";
+import "./style.css";
+
+export const Body = () => {
+  return (
+    <div className="body">
+      <div className="background-image">
+        <div className="microbio" />
+        <div className="overlay-overlayblur" />
+      </div>
+      <div className="background" />
+      <div className="overlay-blur" />
+      <div className="main-login-container">
+        <div className="login-card">
+          <div className="brand-header">
+            <div className="margin">
+              <div className="container">
+                <div className="heading">
+                  <div className="text-wrapper">MicroBio</div>
+                </div>
+              </div>
+            </div>
+            <div className="div" />
+          </div>
+          <div className="login-form">
+            <div className="email-field">
+              <div className="label-e-mail">E-MAIL</div>
+              <div className="container-2">
+                <div className="input">
+                  <input
+                    className="container-3"
+                    placeholder="email@email.com"
+                    type="email"
+                  />
+                </div>
+                <div className="icon-wrapper">
+                  <img className="icon" alt="Icon" src={icon} />
+                </div>
+              </div>
+            </div>
+            <div className="password-field">
+              <div className="label-wrapper">
+                <div className="label">
+                  <div className="senha">SENHA</div>
+                </div>
+              </div>
+              <div className="container-4">
+                <div className="container-wrapper">
+                  <input
+                    className="container-3"
+                    placeholder="••••••••"
+                    type="text"
+                  />
+                </div>
+                <div className="img-wrapper">
+                  <img className="img" alt="Icon" src={image} />
+                </div>
+                <div className="button">
+                  <div className="container-5">
+                    <img className="icon-2" alt="Icon" src={icon2} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="CTA-button">
+              <div className="text">Entrar</div>
+              <div className="container-6">
+                <img className="icon-3" alt="Icon" src={icon3} />
+              </div>
+            </div>
+          </div>
+          <div className="link">
+            <p className="p">
+              Esqueceu sua senha? Entre em contato com a equipe para acessar:
+            </p>
+            <div className="button-css-transform">
+              <div className="div-wrapper">
+                <div className="text-wrapper-2">Clique aqui</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-legal">
+          <div className="element-microbio">© 2026 MICROBIO</div>
+        </div>
+      </div>
+    </div>
+  );
+};
