f7429276c402681333ecf6b3bf57c4c7e7913c80
Daniel Todao <danielhtodao@gmail.com>
2026-05-06 20:09:04 -0300

    feat: ajustes docker e jogando backend para rodar local

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { AppRoutes } from './src/routes'
-import './src/styles/global.css'
-import './src/styles/agro.css'
+import { AppRoutes } from './microbio-project/frontend/src/routes'
+import './microbio-project/frontend/src/styles/global.css'
+import './microbio-project/frontend/src/styles/agro.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- microbio-project/frontend/src/main.tsx
+++ microbio-project/frontend/src/main.tsx
@@ -2,8 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppRoutes } from "./routes";
-import "./styles/global.css";
-import "./styles/agro.css";
+import './src/styles/global.css';
+import './src/styles/agro.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
